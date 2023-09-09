9e6c4f3e7a367e85ef4b8bcea695c622efb230f3
IsabellaKingson <isabellasophiaking@gmail.com>
2023-09-08 20:57:28 -0500

    Added all the sql files: schema, seeds, and queries

---
+++ db/query.sql
@@ -0,0 +1,10 @@
+SELECT role.id, role.title, department.name AS department, role.salary
+FROM role
+JOIN department 
+ON role.department_id = department.id;
+
+SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM employee e 
+LEFT JOIN employee m ON e.manager_id = m.id
+JOIN role ON e.role_id = role.id
+JOIN department ON role.department_id = department.id;
